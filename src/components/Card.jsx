feff0f2e54aa07876ee7452a0a654e16d16e73d2
SudhaBommakanti <mailme.sudha.k@gmail.com>
2020-11-03 10:18:20 +0100

    Added category data to the card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
+import information from '../information.json';
 
 function Card() {
-    const cards = require('../information.json');
-    console.log("json data:",cards);
-    const listItems = cards.map((card) => 
+    console.log("json data:",information);
+   /*  const listItems = cards.map((card) => 
         <article key={card.id}>
             <h2>card.category</h2>
             <p>card.description</p>
-        </article>     
-    );
+        </article>    
+    );*/  
 
     return (
         
-        <div>
-            <h2>check</h2>
-            {listItems.id}
-        </div>
+        <article>
+            <h2>{information[0].category}</h2>
+            </article>
+           
+        
     );
     
 }
