569b83a8a3e2922dbace675c0d8fd9444a288b3a
SudhaBommakanti <mailme.sudha.k@gmail.com>
2020-11-12 07:10:08 +0100

    added header to the homepage component

--- src/components/HomePage/Header.jsx
+++ src/components/HomePage/Header.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
+// Media assets
+import logoLight from "../../assets/images/logo/logo-light.svg";
 
 function Header() {
-  return <h1>Home Page</h1>;
+  // Render
+  return (
+    <header className={"header"}>
+      <Link to="/">
+        <img src={logoLight} alt="Logo" />
+      </Link>
+
+      <div className="search-bar">
+        <input placeholder="Search" />
+        <Link to="/search-page">Search</Link>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
